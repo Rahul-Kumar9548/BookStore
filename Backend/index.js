5880ef617c5502560430ae0a9142d3b29010198b
Rahul Kumar <rahulkumar290399@gmail.com>
2024-09-22 14:26:12 +0530

    BookStore

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,8 @@ import dotenv from "dotenv";
 import bookRoute from "./routes/book.route.js";
 import userRoute from "./routes/user.route.js";
 
+// import path, { dirname } from "path";
+
 
 const app = express();
 const PORT = process.env.PORT || 4000
@@ -14,9 +16,7 @@ const PORT = process.env.PORT || 4000
 
 dotenv.config();
 
-app.use(cors({
-    origin: ["https://mern-project-1-ashy.vercel.app/", "http://localhost:4000"]
-}));
+app.use(cors());
 
 app.use(bodyParser.json({ limit: "4kb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "4kb" }));
@@ -28,6 +28,17 @@ app.use(cookieParser());
 
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+
+
+// Deployment code:
+
+// if(process.env.NODE_ENV === "Production"){
+//     const dirPath = path.resolve();
+//     app.use(express.static("Frontend/dist"));
+//     app.get("*", (req,res)=>{
+//         res.sendFile(path.resolve(dirPath, "Frontend", "dist", "index.html"));
+//     })
+// }
 
 mongoose.connect(`${process.env.DB_PATH}/${process.env.DB_NAME}`)
     .then(() => {
